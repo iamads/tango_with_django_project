dfc617c931f31185cc2b5989502e5fff845b5f48
ads <ads@bebop.com>
2015-02-13 16:45:27 +0530

    About page update

--- rango/views.py
+++ rango/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.shortcuts import render
 # Create your views here.
+def index(request):
+    context_dict={'boldmessage':"Bold message from dictionary"}
+    return render(request,'rango/index.html', context_dict)
+
+def about(request):
+    context_dict={'newmessage':"Message for about page"}
+    return render(request,'rango/about.html', context_dict)
