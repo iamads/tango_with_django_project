6110c35a649f2c485faadc13451c16b3630a7a71
abhijeet <abhijeet3192@gmail.com>
2015-04-11 00:57:46 +0530

    Added add page functionality to rango app

--- rango/views.py
+++ rango/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rango.models import Category
 from rango.models import Page
 from rango.forms import CategoryForm
+from rango.forms import PageForm
 # Create your views here.
 
 
@@ -27,6 +28,7 @@ def category(request, category_name_slug):
 
         context_dict['pages'] = pages
         context_dict['category'] = category
+        context_dict['category_name_slug'] = category_name_slug
     except Category.DoesNotExist:
         pass
 
@@ -45,3 +47,27 @@ def add_category(request):
     else:
         form = CategoryForm()
     return render(request, 'rango/add_category.html', {'form': form})
+
+
+def add_page(request, category_name_slug):
+    try:
+        cat = Category.objects.get(slug=category_name_slug)
+    except Category.DoesNotExist:
+        cat = None
+
+    if request.method == 'POST':
+        form = PageForm(request.POST)
+        if form.is_valid():
+            if cat:
+                page = form.save(commit=False)
+                page.category = cat
+                page.views = 0
+                page.save()
+                return category(request, category_name_slug)
+        else:
+            print form.errors
+    else:
+        form = PageForm()
+
+    context_dict = {'form': form, 'category': cat, 'category_name_slug': category_name_slug}
+    return render(request, 'rango/add_page.html', context_dict)
